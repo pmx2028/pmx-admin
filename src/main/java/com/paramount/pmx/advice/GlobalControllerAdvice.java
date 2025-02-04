1cb21022d075c958197162f8ccfc5bbf426bde7e
hjh <bravehan77@gmail.com>
2025-02-04 19:29:13 +0900

    pmx_admin 커밋

--- src/main/java/com/paramount/pmx/advice/GlobalControllerAdvice.java
+++ src/main/java/com/paramount/pmx/advice/GlobalControllerAdvice.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @ControllerAdvice
-public class GlobalControllerAdvice { dddd
+public class GlobalControllerAdvice {
     @Value("${spring.profiles.active}")
     private String env;
 
